6955ac78b4d8c1bd1272dcf0654ec65401ccee87
OsoConmigo <b.scott.rollan@gmail.com>
2019-01-21 18:44:34 -0500

    git@github.com:scottrollan/Employee-List.git

---
+++ unsolved/app.js
@@ -0,0 +1,21 @@
+// $('.content').html('<p>text added!!!</p>');
+
+// $('.top').html('<p>Look at this!</p>');
+
+// $('.content').addClass('myStyle');
+// $('.content').removeClass('myStyle');
+
+// $('.content').toggleClass('myStyle');
+// $('.top').toggleClass('myStyle');
+
+// $('.content').empty();
+
+// $('.input1').val(4);
+
+//For use with the .on function below
+
+// const sayHello = function() {
+//     console.log("Hello from the click listener");
+// }
+
+// $('.input1').on('click', sayHello);
